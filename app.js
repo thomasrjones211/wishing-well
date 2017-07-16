af8532d038fbe534aa4d1cdd385dcfcb769ea472
Thomas Jones <thomasrjones211@gmail.com>
2017-07-16 17:30:40 -0600

    Inital commit as a target

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+var app = express()
+var port = process.env.PORT || 3000;
+let wishes = []
+
+
+app.set("view engine", "hbs")
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const title = "Wishing Well"
+  res.render("index", {title});
+})
+
+app.get("/form", (req, res) => {
+  res.render("form", {title: "Cast a coin, make a wish"});
+})
+
+app.get("/processForm", (req, res) => {
+  console.log(req.query);
+  wishes.push(req.query);
+  res.render("wishes", {title: "Close your eyes later, and type your wish", wishes});
+})
+
+app.get("/reset", (req, res) => {
+  wishes = [];
+  res.redirect("/")
+})
+
+app.listen(port, function() {
+  console.log("yeah, yeah... on port 3000")
+})
